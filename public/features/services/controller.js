a1369026809e709aa4d7762209ed70fd8d3bb871
ntopno1 <jongwoo.lee@mail.utoronto.ca>
2015-01-20 21:51:41 -0500

    commit

--- public/features/services/controller.js
+++ public/features/services/controller.js
@@ -6,19 +6,29 @@ function ServicesCtrl($scope, $http){
 	$scope.message = "Hello from controller"
 
 	$scope.create = function(){ //sends to the server.js
-		console.log("input from user: ", $scope.serviceClient);
-		$http.post("/serviceClients", $scope.serviceClient); //POST to the server
+		console.log($scope.serviceClient);
+		$http.post("/serviceClients", $scope.serviceClient) //POST to the server
+		.success(function(response){
+			$scope.all()
+		});
 	}
 	
 	$scope.renderServiceClients = function(response){ //receives response from the server.js	
-		console.log("response: ", response)
 		$scope.serviceClients = response;
 		
 	}
 	
+	$scope.remove = function(id){
+		$http.delete("/serviceClients/" + id)
+		.success(function(response){
+			$scope.all()
+		});
+	}
 	//get all
+	$scope.all = function(){
 	$http.get("/serviceClients")	//url where it responds; GET from server
 	.success($scope.renderServiceClients);
+	}
 
-	
+	$scope.all();
 }
--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 var express = require('express'); //import express library
+var mongojs = require('mongojs'); //Mongojs database
+var db = mongojs("ece496", ["serviceClients"]); //name of the db = ece496
+
+
 var app = express();
+
+
+
 
 app.use(express.static(__dirname + '/public')); //dirname : current local directory where node.js is running
 app.use(express.bodyParser());
@@ -7,15 +14,29 @@ app.use(express.bodyParser());
 // GET
 // sends data to UI
 app.get("/serviceClients", function(req, res){
-	var serviceClients = ["test1", "test2", "test3"];
-	res.json(serviceClients);
+	db.serviceClients.find(function(err,docs){
+		res.json(docs);
+	});
 });
 
 app.post("/serviceClients", function(req, res){
 	var svc = req.body;
 	console.log(svc);
+	
+	db.serviceClients.insert(req.body, function(err, doc){
+		res.json(doc);
+	}); // maps to var db = mongojs("ece496", ["serviceClients"])
 });
 
+app.delete("/serviceClients/:id", function (req,res){
+	var id= req.params.id;
+	console.log(id);
+	db.serviceClients.remove({_id : mongojs.ObjectId(id)},
+		function(err,doc){
+			res.json(doc);
+		}
+	)
+});
 
 app.listen(3000);
 
